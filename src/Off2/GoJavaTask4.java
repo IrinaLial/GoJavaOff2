f1acbb0e9a3b93375bbc0a4c87b8afcb38f6b921
Iryna Lialenko <lyalenko.iryna@gmail.com>
2017-09-12 13:04:15 +0300

    task4

--- src/Off2/GoJavaTask4.java
+++ src/Off2/GoJavaTask4.java
@@ -1,4 +1,20 @@
 package Off2;
 
 public class GoJavaTask4 {
+    public static void main(String[]args){
+
+
+        int a= 100;
+        int b =300;
+
+        //равны ли эти числа между собой
+        System.out.println("a == b = " + (a == b) );
+
+        //делится ли A на B без остатка
+        System.out.println("a > b = " + (a > b ) );
+
+        //A больше чем B (true / false)
+        System.out.println("a % b = " + (a % b ==0) );
+
+    }
 }
