030f8024dbf0ede7c9b2fd2bc5c14564e30fe776
Iryna Lialenko <lyalenko.iryna@gmail.com>
2017-09-12 13:00:00 +0300

    Add "a"

--- src/Off2/GoJavaTask3.java
+++ src/Off2/GoJavaTask3.java
@@ -1,4 +1,17 @@
 package Off2;
+import java.util.Scanner;
 
 public class GoJavaTask3 {
+    public static void main (String []args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Task #3" );
+
+        double r = scan.nextDouble();
+        double d = scan.nextDouble ();
+        double e = scan.nextDouble();
+
+        double p = (r+d+e)/2;
+        double s = Math.sqrt((p*(p-r)+(p-d)+(p-e)));
+        System.out.printf("Square of rectangle: %.2f\n", s);
+    }
 }
